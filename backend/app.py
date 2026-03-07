3df99b3f4273e3938412e22863398f77a1b8de61
Ashley Prado <139910977+ashleyprado@users.noreply.github.com>
2026-03-07 18:51:36 -0500

    backend changed to postgreSQL

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
-import sqlite3
+import psycopg2
 
 def get_inventory():
-    conn = sqlite3.connect("../database/warehouse.db")
+    # replace "ashleyprado" with your local postgreSQL username
+    conn = psycopg2.connect(
+        dbname="sequel",
+        user="ashleyprado",
+        host="localhost",
+        port="5432"
+    )
+
     cursor = conn.cursor()
 
-    cursor.execute("SELECT name, quantity_on_hand FROM inventoryitem")
+    cursor.execute("""
+        SELECT name, quantity_on_hand
+        FROM inventoryitem
+    """)
+
     rows = cursor.fetchall()
 
     print("Inventory Report")
@@ -12,7 +23,9 @@ def get_inventory():
     for name, quantity in rows:
         print(f"{name}: {quantity} units")
 
+    cursor.close()
     conn.close()
+
 
 if __name__ == "__main__":
     get_inventory()
